e68f68e719f2b4c5d599db974e044d67dd17e466
gascarcella <scarcella.ga@gmail.com>
2018-03-23 15:45:24 -0300

    Agregando App Soledad

---
+++ src/i4speech/urls.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
+urlpatterns = [
+    # url(r'^$', views.index, name='help'),
+    # url('^textos/$', views.TextosListView.as_view(), name='textos'),
+    # url('^nuevotexto/$', views.TextoNuevoView, name='nuevotexto'),
+    # url('^autores/$', views.AutoresListView.as_view(), name='autores'),
+    # url('^nuevoautor/$', views.AutorNuevoView, name='nuevoautor'),
+    # url('^autores/$', views.AutoresListView.as_view(), name='autores'),
+    # url('^resultados/$', views.ResultadosView, name='resultados'),
+    # url('^resultadoaso/$', views.ResultadoasoView, name='resultadoaso'),
+    # url('^textodetalle/(<pk>)/$', views.TextoDetailView.as_view(), name='textodetalle'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
