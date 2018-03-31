1ce0141bd1b8e3334e61ca0c4162282b645977e8
Soledad <soledadescobar@yahoo.com>
2018-03-31 15:15:30 -0300

    Merge remote-tracking branch 'src/i4speech_app/master'
    # Conflicts:
    #	src/i4speech_app/templates/i4speech_app/base.html
    #	src/i4speech_app/urls.py

--- src/i4speech_app/views.py
+++ src/i4speech_app/views.py
@@ -86,7 +86,7 @@ def DashboardView(request, chartID='chart_ID', chart_type='column', chart_height
 
     chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
     title = {"text": 'Promedio por Autor y Tipo de Indice'}
-    xAxis = {"title": {"text": 'Autor'}, "categories": data['autor']}
+    xAxis = {"title": {"text": 'Autor'}, "categories": data['autor'.encode('utf-8')]}
     yAxis = {"title": {"text": 'Valor'}}
 
     series = [
