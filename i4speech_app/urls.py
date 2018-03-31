686c08a0dcb83ed5075a2a15a03cdbb8a1cca130
Soledad <soledadescobar@yahoo.com>
2018-03-31 08:42:39 -0300

    i4speech

--- i4speech_app/urls.py
+++ i4speech_app/urls.py
@@ -5,30 +5,30 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  url('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, url
+    2. Add a URL to urlpatterns:  url('blog/', include('blog.urls'))
 """
-from django.urls import path
+
 from . import views
 from django.conf.urls import url
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static, url
 from i4speech.settings import DEBUG
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('textos/', views.TextosListView.as_view(), name='textos'),
-    path('nuevotexto/', views.TextoNuevoView, name='nuevotexto'),
-    path('autores/', views.AutoresListView.as_view(), name='autores'),
-    path('nuevoautor/', views.AutorNuevoView, name='nuevoautor'),
-    path('autores/', views.AutoresListView.as_view(), name='autores'),
-    path('resultados/', views.ResultadosView, name='resultados'),
-    path('resultadoaso/', views.ResultadoasoView, name='resultadoaso'),
-    path('textodetalle/(<pk>)/', views.TextoDetailView.as_view(), name='textodetalle'),
-    path('dashboard', views.DashboardView, name='dashboard'),
+    url(r'^$', views.index, name='index'),
+    url(r'^textos/$', views.TextosListView.as_view(), name='textos'),
+    url(r'^nuevotexto/$', views.TextoNuevoView, name='nuevotexto'),
+    url(r'^autores/$', views.AutoresListView.as_view(), name='autores'),
+    url(r'^nuevoautor/$', views.AutorNuevoView, name='nuevoautor'),
+    url(r'^autores/$', views.AutoresListView.as_view(), name='autores'),
+    url(r'^resultados/$', views.ResultadosView, name='resultados'),
+    url(r'^resultadoaso/$', views.ResultadoasoView, name='resultadoaso'),
+    url(r'^textodetalle/(<pk>)/$', views.TextoDetailView.as_view(), name='textodetalle'),
+    url(r'^dashboard/$', views.DashboardView, name='dashboard'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
