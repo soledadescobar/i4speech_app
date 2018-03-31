999f6536775dfd317964bd2eadfef45f9aeb8a3b
Soledad <soledadescobar@yahoo.com>
2018-03-31 13:54:02 -0300

    Merge remote-tracking branch 'src/i4speech_app/master'
    # Conflicts:
    #	src/i4speech_app/templates/i4speech_app/base.html
    #	src/i4speech_app/urls.py

--- src/i4speech_app/chartdata.py
+++ src/i4speech_app/chartdata.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .models import Textos, Autores, Escalafh, Escalamu, Escalasp, Escalagu, Escalain, Cr, Fh, Gu, Mu, Sp
-import django.utils.encoding
+import django.utils
 
 
 class ChartData():
@@ -12,7 +12,7 @@ class ChartData():
         autores = Autores.objects.all()
 
         for autor in autores:
-            data['autor'].append(str(autor.nombre))
+            data['autor'].append(autor.nombre.encode('utf-8'))
             data['sp'].append(Sp.prom_sp(autor.id))
             data['fh'].append(Fh.prom_fh(autor.id))
             data['gu'].append(Gu.prom_gu(autor.id))
